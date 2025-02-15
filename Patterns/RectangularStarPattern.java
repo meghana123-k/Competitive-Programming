f49f1bd308d210fbf35f3731d2ab5f7f9ea4c929
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-02-15 11:42:17 +0530

    RectangularStarPattern.java

--- Patterns/RectangularStarPattern.java
+++ Patterns/RectangularStarPattern.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class RectangularStarPattern {
+    public static void printPattern(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        printPattern(n);
+    }
+}
