869eb9474e88b2af0c68b3fe58ab44c841d9bfc1
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-02-16 14:48:46 +0530

    Add files via upload

---
+++ Patterns/StarPattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        printPattern(n);
+    }
+    public static void printPattern(int n) {
+        for(int i = 0;i<n;i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < (2 * i) + 1; j++) {
+                System.out.print("*");
+            }
+            for(int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
