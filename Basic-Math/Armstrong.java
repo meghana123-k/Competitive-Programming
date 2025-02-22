434fd34d0347463652f0eab95e5e8f3de8d909d2
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-02-22 14:27:04 +0530

    Create Armstrong.java

---
+++ Basic-Math/Armstrong.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    System.out.print("Enter n: ");
+		int n = sc.nextInt();
+		String st = String.valueOf(n);
+		int len = st.length();
+		int res = 0;
+		int temp = n;
+		while(temp != 0) {
+			int dig = temp % 10;
+			res += Math.pow(dig, len);
+			temp /= 10; 
+		}
+    // prints if true, if n == res ,else prints false
+		System.out.println(n == res);
+	}
+}
