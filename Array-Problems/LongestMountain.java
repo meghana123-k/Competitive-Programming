fd95348ecd9ebf5f526a9ef523f8dcbf68a2cc30
meghana123-k <kothakondameghana3@gmail.com>
2025-03-15 16:24:48 +0530

    Added The Length of the longest mountain Solution to Array-Problems

---
+++ Array-Problems/LongestMountain.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LongestMountain {
+
+    public static int longestMountain(int[] arr, int n) {
+        int ans = 0, i = 0;
+        while (i < n) {
+            int j = i;
+            if (j + 1 < n && arr[j] < arr[j + 1]) {
+                while (j + 1 < n && arr[j] < arr[j + 1])
+                    j++;
+                if (j + 1 < n && arr[j] > arr[j + 1]) {
+                    while (j + 1 < n && arr[j] > arr[j + 1])
+                        j++;
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+            i = Math.max(j, i + 1);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter " + n + " Elements");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("The length of the longest mountain is "+longestMountain(a, n));    
+    }
+}
