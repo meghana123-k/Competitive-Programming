6cee3612e8b8257400f9cafbb8d9d7e3788557df
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2025-03-15 13:06:46 +0530

    Update ContainsDuplicates.java

--- Array-Problems/ContainsDuplicates.java
+++ Array-Problems/ContainsDuplicates.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class ContainsDuplicates {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter "+n+" Elements");
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            freq.put(a[i], freq.getOrDefault(a[i], 0)+1);
+        }
+        for(int key: freq.keySet()) {
+            int val = freq.get(key);
+            if(val > 1) {
+                System.out.print(key+" ");
+            }
+        }
     }
 }
