0d09caa268df046bd8a969719bf1b465b7d6d6bd
meghana123-k <kothakondameghana3@gmail.com>
2025-06-14 22:47:06 +0530

    Added Basics of Recursion - param and functional

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class Basics {
-  public static void fun(int i, int sum) {
+  public static void fun1(int i, int sum) {
     if(i < 1) {
       System.out.println(sum);
       return ;
     }
-    fun(i-1, sum + i);
+    fun1(i-1, sum + i);
+  }
+  public static int fun2(int n) {
+    if(n == 0) return 0;
+    return n + fun2(n-1);
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    fun(n, 0);
+    fun1(n, 0);
+    System.out.println(fun2(n));
   }
 }
