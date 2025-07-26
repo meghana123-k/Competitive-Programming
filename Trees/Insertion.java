09e9eff85f2464ed860d4011658a34ac01b330d7
meghana123-k <kothakondameghana3@gmail.com>
2025-07-26 12:04:10 +0530

    Created Folder Trees

---
+++ Trees/Insertion.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+class Insertion {
+    public static Node insertRec(Node root, int value) {
+        if(root == null) {
+            return new Node(value);
+        }
+
+        if(value < root.data) {
+            root = insertRec(root.left, value);
+        } else if(value > root.data) {
+            root = insertRec(root.right, value);
+        }
+        return root;
+    }
+    public static Node insertion(int a[]) {
+        
+        Node root = null;
+        for(int value : a) {
+            root = insertRec(root, value);
+        }
+        return root;
+    }
+    public static void inOrder(Node root) {
+        if(root != null) {
+            inOrder(root.left);
+            System.out.print(root.data);
+            inOrder(root.right);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        System.out.println("Enter Elements: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        Node root = insertion(a);
+        inOrder(root);
+    }
+}
