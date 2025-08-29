c43912fddc5d4ef9e18f98cd1cba224509e1c6ce
meghana123-k <kothakondameghana3@gmail.com>
2025-08-29 15:01:24 +0530

    subsequence sum equals to k

--- Recursion/SubsequenceSumK.java
+++ Recursion/SubsequenceSumK.java
@@ -1,19 +1,22 @@
 import java.util.ArrayList;
 
 public class SubsequenceSumK {
-    public static void subsequenceSumk(int idx, ArrayList<Integer> ar, int[] a, int n, int k, int sum) {
+    public static boolean subsequenceSumk(int idx, ArrayList<Integer> ar, int[] a, int n, int k, int sum) {
         if(idx == n) {
             if(sum == k) {
                 System.out.println(ar);
+                return true;
+            } else {
+                return false;
             }
-            return ;
         }
         ar.add(a[idx]);
         sum = sum+a[idx];
-        subsequenceSumk(idx+1, ar, a, n, k, sum);
+        if(subsequenceSumk(idx+1, ar, a, n, k, sum)) return true;
         sum = sum-a[idx];
         ar.removeLast();
-        subsequenceSumk(idx+1, ar, a, n, k, sum);
+        if(subsequenceSumk(idx+1, ar, a, n, k, sum)) return true;
+        return false;
     }
     public static void main(String[] args) {
         ArrayList<Integer> ar = new ArrayList<>();
