019f968c33d70c392a8cf5499ed51a9e7e6bfd12
meghana123-k <kothakondameghana3@gmail.com>
2025-11-13 19:24:30 +0530

    puzzle solving

---
+++ Array-Problems/Puzzle.java
@@ -0,0 +1,9 @@
+public abstract class Puzzle {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Happy Evening " + args[0]);
+        } else {
+            System.out.println("Bye");
+        }
+    }
+}
