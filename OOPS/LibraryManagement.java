9a6f97fcd5923a20f33b4fab4c9a5aca4486a43f
meghana123-k <kothakondameghana3@gmail.com>
2026-04-17 20:51:54 +0530

    implement Library management system: defined abstract class and derived classes

---
+++ OOPS/LibraryManagement.java
@@ -0,0 +1,55 @@
+package OOPS;
+abstract class LibraryItem {
+    int itemId;
+    String title;
+    boolean isAvailable;
+    LibraryItem(int itemId, String title) {
+        this.itemId = itemId;
+        this.title = title;
+        this.isAvailable = true;
+    }
+    abstract void getLoanDonation();
+    void borrowItem() {
+
+    }
+    void returnItem() {
+
+    }
+    void displayDetails() {
+
+    }
+}
+class Book extends LibraryItem {
+    String author;
+    int loanDuration = 14;
+}
+class Magazine extends LibraryItem {
+    int issueNumber;
+    int loanDuration = 7;
+}
+class DVD extends LibraryItem {
+    int duration; // in minutes
+    int loanDuration = 3;
+}
+class User {
+    int userId;
+    String name;
+    
+    void borrowItem(LibraryItem item) {
+        if (item.isAvailable) {
+            item.borrowItem();
+            System.out.println(name + " borrowed " + item.title);
+        } else {
+            System.out.println(item.title + " is not available for borrowing.");
+        }
+    }
+    void returnItem(LibraryItem item) {
+        item.returnItem();
+        System.out.println(name + " returned " + item.title);
+    }
+    void showBorrowedItems() {
+    }
+}
+public class LibraryManagement {
+    
+}
