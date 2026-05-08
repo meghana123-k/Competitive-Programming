38e75089d4254edbf3d972fa25345a112dac1277
meghana123-k <kothakondameghana3@gmail.com>
2026-05-08 14:46:24 +0530

    Remove unused Solution.java and Dynamic Programming.iml files; update InterfaceExample.java to use pi constant in getArea method

--- OOPS/InterfaceExample.java
+++ OOPS/InterfaceExample.java
@@ -1,12 +1,13 @@
 package OOPS;
 interface Shape {
     // by default, all methods in an interface are abstract and public
+    double pi = 3.14; // by default, all variables in an interface are public, static and final
     void getArea();
     void getPerimeter();
 }
 class Circle implements Shape {
     public void getArea() {
-        System.out.println(45);
+        System.out.println(pi);
     }
     public void getPerimeter() {
         System.out.println("hi");
