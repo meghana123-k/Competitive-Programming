60e73aff4b63a4fc6c30ba619b60413a6c09ad4d
meghana123-k <kothakondameghana3@gmail.com>
2026-07-08 19:01:39 +0530

    Add Basics class with main method for basic boolean evaluation

---
+++ Basic-Math/Basics.java
@@ -0,0 +1,8 @@
+class Basics { 
+    public static void main(String... args) {
+        int var1 = 0;
+        int var2 = 5;
+        boolean b = !(var1 > var2);
+            System.out.println(b+" Yes");
+    }
+}
