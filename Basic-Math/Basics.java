3455ab0ec804afe3587c673e7fbb2c90ce15dd4a
meghana123-k <kothakondameghana3@gmail.com>
2026-07-09 20:37:17 +0530

    Format output in Basics.java and add import statement

--- Basic-Math/Basics.java
+++ Basic-Math/Basics.java
@@ -1,8 +1,11 @@
-class Basics { 
+import java.util.*;;
+
+class Basics {
     public static void main(String... args) {
         int var1 = 0;
         int var2 = 5;
         boolean b = !(var1 > var2);
-            System.out.println(b+" Yes");
+        System.out.println(b + " Yes");
+        System.out.println("My name is \rMeghana");
     }
 }
